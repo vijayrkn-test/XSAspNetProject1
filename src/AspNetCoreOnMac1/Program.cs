bcf1ec14cc013e5ac3517878998827e5f87b5030
vijayramakrishnan <vramak@microsoft.com>
2016-07-13 14:40:31 -0700

    remote

---
+++ src/AspNetCoreOnMac1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreOnMac1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+		}
+	}
+}
+
+
